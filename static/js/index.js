a4f623aae3586c312549af28e35f8974714c49e5
Jessica Tang <jynnie@VPN-18-101-16-156.MIT.EDU>
2017-07-29 22:32:00 +0800

    Framework for culture page

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,8 @@
-$(function(){
-	
-	/* Service hovers */
-	$("#social ul li").hover(
-		function() {
-			$("#social").addClass($(this).data("network")).addClass("active");
-			$("#social p").html($(this).data("text"));
-		},
-		function() {
-			$("#social").removeClass();
-			$("#social p").html("Find me on the web");
-		}
-	);
-		
+$(document).ready(function(){
+	$('#culture').on('mouseenter', function () {
+		$("#cultureDrop").addClass('show');
+	});
+	$('#culture').on('mouseleave', function () {
+		$("#cultureDrop").removeClass('show');
+	});
 });
